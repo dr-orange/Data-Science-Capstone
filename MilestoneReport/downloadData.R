3c2b4a9fd1959abcc16270320783556c18df362b
Koji <dr.orange.jr@gmail.com>
2018-07-28 22:38:33 +0900

    finished Task2

--- MilestoneReport/downloadData.R
+++ MilestoneReport/downloadData.R
@@ -2,6 +2,8 @@ downloadData <- function(workingDataPath = file.path("data")) {
         # Download Data
         rawDataFileUrl <- "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip"
         downloadedZipfilePath <- file.path(workingDataPath, "Coursera-SwiftKey.zip")
+        badWordsFileUrl <- "https://www.cs.cmu.edu/~biglou/resources/bad-words.txt"
+        badWordsFilePath <- file.path(workingDataPath, "bad-words.txt")
         basePath <- file.path(workingDataPath, "final", "en_US")
         
         # Create working directory
@@ -12,6 +14,10 @@ downloadData <- function(workingDataPath = file.path("data")) {
         if(!file.exists(downloadedZipfilePath)) {
                 download.file(rawDataFileUrl, destfile = downloadedZipfilePath, method = "curl")
         }
+        # Download Bad Words Text File
+        if(!file.exists(badWordsFilePath)) {
+                download.file(badWordsFileUrl, destfile = badWordsFilePath, method = "curl")
+        }
         # Unzip
         if(!file.exists(basePath)) {
                 unzip(zipfile = downloadedZipfilePath, exdir = workingDataPath)
@@ -19,5 +25,6 @@ downloadData <- function(workingDataPath = file.path("data")) {
         
         list(blogs = file.path(basePath, "en_US.blogs.txt"), 
              twitter = file.path(basePath, "en_US.twitter.txt"),
-             news = file.path(basePath, "en_US.news.txt"))
+             news = file.path(basePath, "en_US.news.txt"),
+             badwords = badWordsFilePath)
 }
